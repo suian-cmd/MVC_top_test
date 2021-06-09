0e2827d9bdbf2b49c8018bb038cda557c83f73d6
suian <1084031114@qq.com>
2021-06-09 21:45:00 +0800

    MVC上小作业，涉及parcel打包问题处理

---
+++ js/app4.js
@@ -0,0 +1,10 @@
+import "../css/app4.css";
+import $ from "jquery";
+
+const $circle = $('#app4 .circle')
+
+$circle.on('mouseenter', ()=>{
+  $circle.addClass('active')
+}).on('mouseleave', ()=>{
+  $circle.removeClass('active')
+})
